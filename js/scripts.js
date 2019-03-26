a422d5871e263d34b120d80580d110a5adcc5118
Anna Gibbons and Tessa Sullivan <tessa@>
2019-03-26 10:05:50 -0700

    added polar bear slide in/out functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#initially-showing").slideToggle();
-    $("#initially-hidden").slideToggle();
+  $(".clickable1").click(function() {
+    $("#initially-showing").slideUp();
+    $("#initially-hidden").slideDown();
+  });
+  $(".clickable3").click(function() {
+    $("#polar-showing").slideUp();
+    $("#polar-hidden").slideDown();
+  });
+  $(".clickable2").click(function() {
+    $("#initially-showing").slideDown();
+    $("#initially-hidden").slideUp();
+  });
+  $(".clickable4").click(function() {
+    $("#polar-showing").slideDown();
+    $("#polar-hidden").slideUp();
   });
 });
